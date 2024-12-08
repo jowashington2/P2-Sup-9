ce659669b01cc748beb945effd79fcd2724b957b
Joshua Washington <jdwashington195@gmail.com>
2024-12-08 13:27:34 -0500

    Implement utility functions and JSDoc

---
+++ StringUtils.java
@@ -0,0 +1,11 @@
+public class StringUtils {
+
+    /**
+     * Converts all characters in the given string to uppercase.
+     * @param input The input string.
+     * @return String The string in uppercase.
+     */
+    public static String convertToUpperCase(String input) {
+        return input.toUpperCase();
+    }
+}
